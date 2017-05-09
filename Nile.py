6b78e29905441b43216dc64c49b88641d00e3452
tayatoshi <tsk0414z@gmail.com>
2017-05-09 11:46:11 +0900

    change y_train to self.length

--- Nile.py
+++ Nile.py
@@ -21,7 +21,7 @@ plt.show()
 class Calculation1:
     def __init__(self):
         pass
-
+    
     def kf(y_train, a0, p0, e, h, Z, T):
         length=len(y_train)
         a = a0
@@ -41,11 +41,11 @@ class Calculation1:
             vero[i] = np.log(F) + (u ** 2)/F
         vero_fit = -0.5*sum(vero)
         return([P_t, A_t, vero_fit, Z*A_t])
-
+    
 class Calculation2:
     def __init__(self):
         pass
-
+    
     #pars(np.array([y,e,h,Z,T]))
     def pars(p):
         y = p[0]
@@ -57,14 +57,16 @@ class Calculation2:
         return(result)
 
 class KF_estimate(Calculation1,Calculation2):
-    def __init__(self,init_e,init_h,init_z,init_t):
+    def __init__(self,data,init_e,init_h,init_z,init_t):
+        self.data = data
+        self.length = len(data)
         re = []
         L=None
         for e in np.arange(init_e[0],init_e[1],init_e[2]):
             for h in np.arange(init_h[0],init_h[1],init_h[2]):
                 for z in np.arange(init_z[0],init_z[1],init_z[2]):
                     for t in np.arange(init_t[0],init_t[1],init_t[2]):
-                        l=Calculation2.pars(np.array([y_train,e,h,z,t]))[2]
+                        l=Calculation2.pars(np.array([data,e,h,z,t]))[2]
                         re.append(l)
                         print('(',e,h,z,t,')',l)
                         if(L==None or l>L):
@@ -80,11 +82,11 @@ class KF_estimate(Calculation1,Calculation2):
         print('-------------------------------------------')
         print('e = ', ep, 'h = ', eta, 'Z = ', Z, 'T = ', T)
         self.estimation={"L":L, "e":ep, "h":eta, "Z":Z, "T":T}
-        self.prediction=Calculation2.pars(np.array([y_train,self.estimation['e'],self.estimation['h'],self.estimation['Z'],self.estimation['T']]))
-
+        self.prediction=Calculation2.pars(np.array([data,self.estimation['e'],self.estimation['h'],self.estimation['Z'],self.estimation['T']]))
+    
     def predict(self):
         return(self.prediction)
-
+    
     def forecast(self, n = 20):
         self.prediction
         forecast_a = np.zeros(n)
@@ -93,10 +95,13 @@ class KF_estimate(Calculation1,Calculation2):
             forecast_a[i] = self.estimation['Z']*forecast_A
             forecast_A = forecast_a[i]
         forecast_a = pd.DataFrame(forecast_a)
-        forecast_a.index = forecast_a.index + len(y_train)
+        forecast_a.index = forecast_a.index + self.length
         return(forecast_a)
-
-    def smooth():
+    
+    def smoothing(self):
+        smoothe=np.zeros(self.length)
+        for i in range(self.length):
+            smooth[i]=1
         return()
 
 
@@ -106,7 +111,7 @@ init_e=np.array([9,10,0.01])
 init_h=np.array([7,8,0.01])
 init_z=np.array([1,2,1])
 init_t=np.array([1,2,1])
-kfe=KF_estimate(init_e,init_h,init_z,init_t)
+kfe=KF_estimate(y_train,init_e,init_h,init_z,init_t)
 
 
 # In[ ]:
@@ -117,10 +122,15 @@ fore=kfe.forecast()
 
 # In[ ]:
 
+plt.plot(y,label='observation')
 plt.plot(pre[1],label='predict')
 plt.plot(fore,label='forecast')
-plt.plot(y,label='observation')
 plt.legend()
 plt.title('Flow of River Nile')
 plt.show()
+
+
+# In[ ]:
+
+
 
