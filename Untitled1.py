11e33001ffc5646bbaf3e5646cd09b53e4c7d5ab
tayatoshi <tsk0414z@gmail.com>
2017-05-17 14:30:55 +0900

    finish

--- Untitled1.py
+++ Untitled1.py
@@ -18,9 +18,14 @@ plt.show()
 
 # In[ ]:
 
-class Calculation1:
-    def __init__(self):
-        pass
+class Kalman():
+    def __init__(self,data,init_e,init_h,init_z,init_t):
+        self.data=data
+        self.length = len(data)
+        self.init_e=init_e
+        self.init_h=init_h
+        self.init_z=init_z
+        self.init_t=init_t
     
     def kf(y_train, a0, p0, e, h, Z, T):
         length=len(y_train)
@@ -41,38 +46,32 @@ class Calculation1:
             vero[i] = np.log(F) + (u ** 2)/F
         vero_fit = -0.5*sum(vero)
         return({'A_t':A_t, 'P_t':P_t, 'vero_fit':vero_fit, 'y_hat':Z*A_t})
-
-
-# In[ ]:
-
-class Calculation2:
-    def __init__(self):
-        pass
     
     #pars(np.array([y,e,h,Z,T]))
-    def pars(p):
+    @classmethod
+    def pars(clf,p):
         y = p[0]
         e = np.exp(p[1])
         h = np.exp(p[2])
         Z = p[3]
         T = p[4]
-        result = Calculation1.kf(y,0.0,10.0**6,e,h,Z,T)
+        result = Kalman.kf(y,0.0,10.0**6,e,h,Z,T)# pars(self,p)にしてもself.kfではできなかった
         return(result)
-
-
-# In[ ]:
-
-class KF_estimate(Calculation1,Calculation2):
-    def __init__(self,data,init_e,init_h,init_z,init_t):
-        self.data = data
-        self.length = len(data)
+    
+    def fit(self):
+        global L
+        global ep
+        global eta
+        global Z
+        global T
+        global estimation
         re = []
         L=None
-        for e in np.arange(init_e[0],init_e[1],init_e[2]):
-            for h in np.arange(init_h[0],init_h[1],init_h[2]):
-                for z in np.arange(init_z[0],init_z[1],init_z[2]):
-                    for t in np.arange(init_t[0],init_t[1],init_t[2]):
-                        l=Calculation2.pars(np.array([data,e,h,z,t]))['vero_fit']
+        for e in np.arange(self.init_e[0],self.init_e[1],self.init_e[2]):
+            for h in np.arange(self.init_h[0],self.init_h[1],self.init_h[2]):
+                for z in np.arange(self.init_z[0],self.init_z[1],self.init_z[2]):
+                    for t in np.arange(self.init_t[0],self.init_t[1],self.init_t[2]):
+                        l=self.pars(np.array([self.data,e,h,z,t]))['vero_fit']
                         re.append(l)
                         print('(',e,h,z,t,')',l)
                         if(L==None or l>L):
@@ -86,33 +85,24 @@ class KF_estimate(Calculation1,Calculation2):
         print('log Likelihood = ', L)
         print('-------------------------------------------')
         print('e = ', ep, 'h = ', eta, 'Z = ', Z, 'T = ', T)
-        self.estimation={"L":L, "e":ep, "h":eta, "Z":Z, "T":T}
-        self.prediction=Calculation2.pars(np.array([data,self.estimation['e'],self.estimation['h'],self.estimation['Z'],self.estimation['T']]))
-    
+        
+        estimation={"L":L, "e":ep, "h":eta, "Z":Z, "T":T}
+        return(estimation)
+        
     def predict(self):
-        return(self.prediction)
+        prediction=self.pars(np.array([self.data,estimation['e'],estimation['h'],estimation['Z'],estimation['T']]))
+        return(prediction)
     
     def forecast(self, n = 20):
-        self.prediction
         forecast_a = np.zeros(n)
-        forecast_A = self.prediction['A_t'][-1]
+        forecast_A = self.predict()['A_t'][-1]
         for i in range(n):
-            forecast_a[i] = self.estimation['Z']*forecast_A
+            forecast_a[i] = estimation['Z']*forecast_A
             forecast_A = forecast_a[i]
         forecast_a = pd.DataFrame(forecast_a)
         forecast_a.index = forecast_a.index + self.length
         return(forecast_a)
-
-# 平滑化は諦めた。
-#    def smoothing(self):
-#        smoothe=np.zeros(self.length)
-#        for i in range(self.length):
-#            if(i==0):
-#                smooth_A[i]=self.predict['A_t'][-(i+1)]
-#                smooth_P[i]=self.predict['P_t'][-(i+1)]
-#            else:
-#                smooth_A[i:=self.predict['A_t'][-(i+1)]+self.predict['P_t'][-(i+1)]*T
-#        return()
+    
 
 
 # In[ ]:
@@ -121,12 +111,24 @@ init_e=np.array([9,10,0.01])
 init_h=np.array([7,8,0.01])
 init_z=np.array([1,2,1])
 init_t=np.array([1,2,1])
-kfe=KF_estimate(y_train,init_e,init_h,init_z,init_t)
+kfe=Kalman(y_train,init_e,init_h,init_z,init_t)
 
 
 # In[ ]:
 
+kfe.fit()
+
+
+# In[ ]:
+
+# In[ ]:
+
 pre=kfe.predict()
+pre
+
+
+# In[ ]:
+
 fore=kfe.forecast()
 
 
@@ -144,6 +146,11 @@ plt.show()
 
 plt.plot(pre['P_t'])
 plt.show()
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
